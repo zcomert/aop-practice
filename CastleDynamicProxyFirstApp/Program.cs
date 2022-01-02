21459b4396c41100e33b42292355b419930bb702
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-01-02 21:51:28 +0300

    Cast Dynamic Proxy First App has been implemented.

---
+++ CastleDynamicProxyFirstApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace CastleDynamicProxyFirstApp
+{
+    public class MyInterceptorAspect : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine("Before {0}",invocation.Method);
+            invocation.Proceed();
+            Console.WriteLine("After {0}",invocation.Method);
+        }
+    }
+    public class MyClass
+    {
+        public virtual void MyMethod()
+        {
+            Console.WriteLine("MyMethod body.");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = new ProxyGenerator();
+            var aspect = proxy
+                .CreateClassProxy<MyClass>(new MyInterceptorAspect());
+
+            aspect.MyMethod();
+            Console.ReadLine();
+        }
+    }
+}
