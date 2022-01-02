ff1fe6668761631f132d7585043febf2f609acba
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-01-03 00:00:38 +0300

    AspectInterceptorSelector has been implemented.

--- CastleDynamicProxyFirstApp/MyClass.cs
+++ CastleDynamicProxyFirstApp/MyClass.cs
@@ -2,9 +2,14 @@
 
 namespace CastleDynamicProxyFirstApp
 {
-    public class MyClass
+    public interface IMyClass
     {
-        [MyInterceptorAspect]
+        void MyMethod();
+    }
+    
+    public class MyClass : IMyClass
+    {
+        [MyInterceptorAspect(Priority = 1)]
         public virtual void MyMethod()
         {
             Console.WriteLine("MyMethod body.");
--- CastleDynamicProxyFirstApp/Program.cs
+++ CastleDynamicProxyFirstApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using Castle.DynamicProxy;
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using Core.Interceptors;
 using System;
 
 namespace CastleDynamicProxyFirstApp
@@ -11,8 +14,22 @@ namespace CastleDynamicProxyFirstApp
             var proxy = new ProxyGenerator();
             var aspect = proxy
                 .CreateClassProxy<MyClass>(new MyInterceptorAspect());
-
             aspect.MyMethod();
+
+            Console.WriteLine(new String('-',50));
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MyClass>()
+                .As<IMyClass>()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions() { 
+                    Selector = new AspectInterceptorSelector()
+                })
+                .InstancePerDependency();
+
+            var container = builder.Build();
+            var willBeIntercepted = container.Resolve<IMyClass>();
+            willBeIntercepted.MyMethod();
+
             Console.ReadLine();
         }
     }
